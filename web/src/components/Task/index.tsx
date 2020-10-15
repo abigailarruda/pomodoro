7c6062014a2668cdc0c906b816d24ed50299c172
Abigail Arruda <itssabbie@gmail.com>
2020-10-14 21:13:46 -0300

    Criando componente Task

--- web/src/components/Task/index.tsx
+++ web/src/components/Task/index.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./styles.css";
+
+import Icon from "@mdi/react";
+import {
+  mdiCalendarMonth,
+  mdiClockTimeThreeOutline,
+  mdiDotsHorizontal,
+  mdiDelete,
+  mdiPencil,
+  mdiCheck,
+} from "@mdi/js";
 
 interface TaskProps {
   text: String;
@@ -10,7 +20,67 @@ interface TaskProps {
 }
 
 const Task: React.FC<TaskProps> = ({ text, date, time, isDone }) => {
-  return <h1>Task</h1>;
+  function getClasses() {
+    if (isDone) {
+      return "card-text done";
+    } else {
+      return "card-text";
+    }
+  }
+  return (
+    <div className="card">
+      <div className="card-body">
+        <p className={getClasses()}>{text}</p>
+      </div>
+      <div className="card-footer d-flex justify-content-between">
+        <div className="d-flex justify-content-start flex-row">
+          <p className="d-flex align-items-center mb-0">
+            <Icon path={mdiCalendarMonth} size={0.7} color="#e1e1e1" />
+            <span className="ml-2">{date}</span>
+          </p>
+          <p className="d-flex align-items-center ml-3 mb-0">
+            <Icon path={mdiClockTimeThreeOutline} size={0.7} color="#e1e1e1" />
+            <span className="ml-2">{time}</span>
+          </p>
+        </div>
+        <div className="justify-content-end">
+          <button
+            className="btn btn-dots dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <Icon path={mdiDotsHorizontal} size={0.8} color="#e1e1e1" />
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a
+              className="dropdown-item d-flex align-items-center"
+              type="button"
+            >
+              <Icon path={mdiCheck} size={0.7} color="#e1e1e1" />
+              <span className="ml-2">Mark as done</span>
+            </a>
+            <a
+              className="dropdown-item d-flex align-items-center"
+              type="button"
+            >
+              <Icon path={mdiPencil} size={0.7} color="#e1e1e1" />
+              <span className="ml-2">Edit task</span>
+            </a>
+            <a
+              className="dropdown-item d-flex align-items-center"
+              type="button"
+            >
+              <Icon path={mdiDelete} size={0.7} color="#e1e1e1" />
+              <span className="ml-2">Delete task</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Task;
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -16,6 +16,8 @@ import {
   mdiMenu,
 } from "@mdi/js";
 
+import Task from "../../components/Task";
+
 function Landing() {
   return (
     <div className="d-flex toggled" id="wrapper">
@@ -28,31 +30,31 @@ function Landing() {
 
         <div className="list-group list-group-flush">
           <div className="list-group-item">
-            <button className="btn btn-sidebar-active">
+            <button className="btn btn-sidebar-active" type="button">
               <Icon path={mdiTimer} size={0.7} color="#FAFAFA" />
               <span>Tracker</span>
             </button>
           </div>
           <div className="list-group-item">
-            <button className="btn btn-sidebar">
+            <button className="btn btn-sidebar" type="button">
               <Icon path={mdiAccount} size={0.7} color="#e0e0e0" />
               <span>Account</span>
             </button>
           </div>
           <div className="list-group-item">
-            <button className="btn btn-sidebar">
+            <button className="btn btn-sidebar" type="button">
               <Icon path={mdiCog} size={0.7} color="#e0e0e0" />
               <span>Settings</span>
             </button>
           </div>
           <div className="list-group-item">
-            <button className="btn btn-sidebar">
+            <button className="btn btn-sidebar" type="button">
               <Icon path={mdiEmail} size={0.7} color="#e0e0e0" />
               <span>Contact</span>
             </button>
           </div>
           <div className="list-group-item">
-            <button className="btn btn-sidebar">
+            <button className="btn btn-sidebar" type="button">
               <Icon path={mdiLogoutVariant} size={0.7} color="#e0e0e0" />
               <span>Log Out</span>
             </button>
@@ -63,17 +65,39 @@ function Landing() {
       <div id="page-content-wrapper">
         <nav className="navbar navbar-expand-lg">
           <button
-            className="btn btn-menu d-flex d-sm-flex d-md-flex d-lg-none d-xl-none"
             id="menu-toggle"
             onClick={() => {
               $("#wrapper").toggleClass("toggled");
             }}
+            className="btn btn-menu d-flex d-sm-flex 
+            d-md-flex d-lg-none d-xl-none"
           >
             <Icon path={mdiMenu} size={1} color="#e1e1e1" />
           </button>
         </nav>
 
-        <div className="container-fluid"></div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-xs-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 offset-md-1 offset-lg-1 offset-xl-1 order-md-last">
+              {/* Timer */}
+            </div>
+            <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 offset-md-1 offset-lg-1 offset-xl-1 order-xs-last order-md-first">
+              <Task
+                text="Design an interactive receipt for a historic landmark."
+                date="14/10/2020"
+                time="20:49"
+                isDone={false}
+              />
+              <Task
+                text="Create a design to quickly and easily view important information about your home from a remote location."
+                date="14/10/2020"
+                time="20:00"
+                isDone={true}
+              />
+            </div>
+            <div className="offset-md-1 offset-lg-1 offset-xl-1"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
